f3d670bbd98b7b37b6f945f199943c9373a5c441
Mariana Madeira <mrn.m.92@gmail.com>
2021-12-06 10:57:46 -0300

    Aplicado tema Bootstrap de Bootswatch

---
+++ output/app.js
@@ -0,0 +1,10 @@
+"use strict";
+console.log("holi mundis!");
+let a; // Inferencia explícita
+let b; // Inferencia explícita
+let c = 101; // Inferencia implícita
+let recursos = ['memoria', 'disco', 'procesador', 100];
+let recursos2 = ['memoria', 'disco', 'procesador'];
+console.log(c);
+let message = 'Hello World';
+console.log(message);
--- script.ts
+++ script.ts
@@ -1,1 +1,13 @@
 console.log("holi mundis!");
+
+let a: number; // Inferencia explícita
+let b: string; // Inferencia explícita
+let c = 101;   // Inferencia implícita
+
+let recursos = ['memoria', 'disco', 'procesador', 100];
+let recursos2:string [] = ['memoria', 'disco', 'procesador'];
+
+console.log(c);
+
+let message: string = 'Hello World';
+console.log(message);
